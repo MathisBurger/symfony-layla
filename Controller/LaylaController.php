2e211fe72327237467a4b334f7afd177109d8265
Mathis Burger <57145815+MathisBurger@users.noreply.github.com>
2022-10-22 18:13:28 +0200

    Update LaylaController.php

--- Controller/LaylaController.php
+++ Controller/LaylaController.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * This file is part of the nwsnet/core package
- *
- * © 2022 Die NetzWerkstatt GmbH & Co. KG <info@die-netzwerkstatt.de>
- *
- * For the full copyright and license information, please read the
- * LICENSE file that was distributed with this source code.
- */
 
 namespace MathisBurger\SymfonyLayla\Controller;
 
